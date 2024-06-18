047771977a02eb47bc52b17608a65d5ca1a72b3e
KotegarChaithra <chaithrakotegar8197@gmail.com>
2024-06-18 00:51:17 -0700

    basic is statement

---
+++ if.py
@@ -0,0 +1,10 @@
+## We will use if when set of conditions to be happening
+## Example :         statement 1
+##                   statement 2
+##                   statement 3
+##    statement 4                  statement 7
+##    statement 5                  statement 8
+##    statement 6                  statement 9
+##
+if True:
+    print("I'm right")

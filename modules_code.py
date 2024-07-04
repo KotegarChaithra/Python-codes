95010c7c200999918ec680d6975a9c51e5df800a
KotegarChaithra <155987898+KotegarChaithra@users.noreply.github.com>
2024-07-04 01:35:19 -0700

    Add files via upload
    how to use modules which are created by me

---
+++ modules_code.py
@@ -0,0 +1,12 @@
+from modules import add, sub,mul,div
+#import modules
+a =6
+b = 7
+c= sub(a,b)
+print("subtraction:",c)
+d=add(a,b)
+print("Addition:",d)
+e=mul(a,b)
+print("Multipliction:",e)
+f=div(a,b)
+print("Division:",f)
